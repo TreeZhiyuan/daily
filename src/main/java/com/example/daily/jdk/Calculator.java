8629570c407480e52fc3eeff306d357a1a26170b
zhiyuan cui <cuizhiyuan@lianqiangroup.com>
2018-04-11 16:30:47 +0800

    add some test case on jdk8

---
+++ src/main/java/com/example/daily/jdk/Calculator.java
@@ -0,0 +1,26 @@
+package com.example.daily.jdk;
+
+/**
+ * @author: zhiyuan
+ * @date: 2018-04-11
+ * @project: daily
+ * @description:
+ */
+
+public class Calculator {
+
+	interface IntegerMath {
+		int operation(int a, int b);
+	}
+
+	public int operateBinary(int a, int b, IntegerMath op) {
+		return op.operation(a, b);
+	}
+
+	public static void main(String... args) {
+		Calculator myApp = new Calculator();
+		System.out.println("40 + 2 = " + myApp.operateBinary(40, 2, (a, b) -> a + b));
+		System.out.println("20 - 10 = " + myApp.operateBinary(20, 10, (a, b) -> a - b));
+	}
+
+}
