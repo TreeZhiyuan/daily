3761e4a997cb72c1e6d7cbfef54fd61955711da5
zhiyuan cui <cuizhiyuan@lianqiangroup.com>
2018-05-23 14:39:58 +0800

    add some comments about Lambda Expressions

--- src/main/java/com/example/daily/jdk/Calculator.java
+++ src/main/java/com/example/daily/jdk/Calculator.java
@@ -7,25 +7,25 @@ import java.util.Date;
  * @author: zhiyuan
  * @date: 2018-04-11
  * @project: daily
- * @description:
+ * @description:将接口的实现作为参数传到方法中,有点类似与JavaScript的函数传递
  */
 
 public class Calculator {
 
-	interface IntegerMath {
-		int operation(int a, int b);
-	}
+    interface IntegerMath {
+        int operation(int a, int b);
+    }
 
-	public int operateBinary(int a, int b, IntegerMath op) {
-		return op.operation(a, b);
-	}
+    public int operateBinary(int a, int b, IntegerMath op) {
+        return op.operation(a, b);
+    }
 
-	public static void main(String... args) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
-		System.out.println(sdf.format(new Date()) + ">" + ((int)(Math.random()*900) + 100) + "");
-		Calculator myApp = new Calculator();
-		System.out.println("40 + 2 = " + myApp.operateBinary(40, 2, (a, b) -> a + b));
-		System.out.println("20 - 10 = " + myApp.operateBinary(20, 10, (a, b) -> a - b));
-	}
+    public static void main(String... args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
+        System.out.println(sdf.format(new Date()) + ">" + ((int) (Math.random() * 900) + 100) + "");
+        Calculator myApp = new Calculator();
+        System.out.println("40 + 2 = " + myApp.operateBinary(40, 2, (a, b) -> a + b));
+        System.out.println("20 - 10 = " + myApp.operateBinary(20, 10, (a, b) -> a - b));
+    }
 
 }
